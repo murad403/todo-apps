fcbf62611a26d6d31169783a3f90444e9665c083
vanhellsing102 <mdm78774@gmail.com>
2025-10-18 04:39:31 -0600

    todo apps project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h1>My ToDo Apps</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
+
+export default App;
