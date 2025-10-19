65e978699e559aa5cd1a224b3320a25816e31fed
vanhellsing102 <mdm78774@gmail.com>
2025-10-18 23:43:09 -0600

    todo modal and filter

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,14 +1,21 @@
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDelete } from "react-icons/md";
+import { Button } from "../ui/button";
 
 const TodoCard = () => {
     return (
-        <div className='bg-gray-200 py-1 px-3 rounded-md flex justify-between items-center text-sm'>
+        <div className='py-1 px-3 border border-gray-300 rounded-sm flex justify-between items-center text-sm'>
             <input type="checkbox" id='' name='' />
             <h3 className="font-semibold text-[16px]">Todo title</h3>
             <p>time</p>
             <p>description</p>
-            <div className="space-x-2">
-                <button>Edit</button>
-                <button>Delete</button>
+            <div className="space-x-2 *:cursor-pointer">
+                <Button className="bg-violet-500">
+                    <CiEdit/>
+                </Button>
+                <Button className="bg-red-500">
+                    <MdOutlineDelete/>
+                </Button>
             </div>
         </div>
 
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,14 +1,23 @@
 import TodoCard from "@/components/todo/TodoCard";
+import TodoFilter from "@/components/todo/TodoFilter";
+import TodoModal from "@/components/todo/TodoModal";
+
 
 
 const Todo = () => {
     return (
         <div className="h-screen w-full max-w-6xl mx-auto">
             <h2 className="text-3xl font-semibold text-center my-5">My Todo Apps</h2>
-            <div className=" bg-red-500 p-3 rounded-md space-y-3">
-                <TodoCard></TodoCard>
-                <TodoCard></TodoCard>
-                <TodoCard></TodoCard>
+            <div className="mb-3 flex justify-between items-center">
+                <TodoModal></TodoModal>
+                <TodoFilter></TodoFilter>
+            </div>
+            <div className="bg-gradient-to-l to-violet-500 from-red-600 p-2 rounded-lg">
+                <div className="space-y-3 bg-white rounded-sm p-2">
+                    <TodoCard></TodoCard>
+                    <TodoCard></TodoCard>
+                    <TodoCard></TodoCard>
+                </div>
             </div>
         </div>
     );
